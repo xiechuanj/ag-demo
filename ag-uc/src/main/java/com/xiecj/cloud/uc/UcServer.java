501e2c1f27620b65798953c4f6a5a4519eb447f6
xiechuanj <xiechuanj@gmail.com>
2018-07-29 19:29:11 +0800

    add jdbc

--- ag-uc/src/main/java/com/xiecj/cloud/uc/UcServer.java
+++ ag-uc/src/main/java/com/xiecj/cloud/uc/UcServer.java
@@ -1,5 +1,6 @@
 package com.xiecj.cloud.uc;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -9,6 +10,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableEurekaClient
+@MapperScan("com.xiecj.cloud.uc.mapper")
 public class UcServer {
 
     public static void main(String[] args){
