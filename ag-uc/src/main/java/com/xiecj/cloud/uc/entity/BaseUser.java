4245f97c83203b1fe2a21fc8ed0e5c332d2f04da
Your Name <you@example.com>
2018-07-30 16:19:14 +0800

    add mapper

---
+++ ag-uc/src/main/java/com/xiecj/cloud/uc/entity/BaseUser.java
@@ -0,0 +1,39 @@
+package com.xiecj.cloud.uc.entity;
+
+import javax.persistence.*;
+
+@Table(name = "user")
+public class BaseUser {
+    @Id
+    private Integer id;
+
+    private String name;
+
+    /**
+     * @return id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ ag-uc/src/main/java/com/xiecj/cloud/uc/mapper/BaseUserMapper.java
@@ -0,0 +1,7 @@
+package com.xiecj.cloud.uc.mapper;
+
+import com.xiecj.cloud.uc.entity.BaseUser;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface BaseUserMapper extends Mapper<BaseUser> {
+}
